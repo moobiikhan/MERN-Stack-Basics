75316d77fc657323e624ab5eba1282b167f06b33
moobiikhan <81070304+moobiikhan@users.noreply.github.com>
2021-03-26 01:16:25 +0500

    Create app2.js

---
+++ app2.js
@@ -0,0 +1,24 @@
+var express = require("express")
+var app = express();
+var R1 = require("./routemod")
+var R2 = require("./routebook")
+
+// this is first web avi
+// this is a curd operation for the given route
+//http://localhost:3000/books/
+// this is CRUD create, read, update and delete.
+
+app.use("/books",R2)
+
+app.use("/students",R1)
+
+var Server= app.listen(3000, ()=> {
+    console.log("This route is running at 3000")
+    })
+
+
+
+// we created a server and a route in thhis and them we created the
+// methods of route the curd operations, but if we have so many routes and
+// this code will be messy so we'll use another file for the methords(CRUD) 
+// over to routemod.js
