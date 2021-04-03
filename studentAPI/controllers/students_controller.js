088a779fdf561036d3cb54ec15ee5e46b449f18d
moobiikhan <81070304+moobiikhan@users.noreply.github.com>
2021-04-03 05:06:43 +0500

    Add files via upload

--- studentAPI/controllers/students_controller.js
+++ studentAPI/controllers/students_controller.js
@@ -7,21 +7,32 @@ exports.test= (req,res)=>{
 
 exports.create= (req,res,next)=>{
     let student = new student2({
-        name: req.params.name,
-        id: req.params.id,
-        dept: req.params.dept
+        name: req.body.name,
+        id: req.body.id,
+        dept: req.body.dept
     })
 
-        student.save((err,)=>{
+        student.save((err)=>{
             if(err){ return next(err)}
             res.send("Student Added to the Data!")
          
         })}
 
-exports.delete(()=>{})
+exports.delete= (req,res)=>{
+    student.findByIdAndRemove(req.params.id, (err)=>{
+        if (err) return next(err);
+        res.send("Object Deleted!!!!!")
+})}
 
-exports.put(()=>{})
+exports.put= (req,res)=>{
+    student.findById(req.params.id, (err,student)=>{
+        if(err) return next(err);
+        res.send(student)
+    })}
 
 
-
-    
+exports.update=(req,res)=>{
+    student.findByIdAndUpdate(req.params.id, {$set: req.body}, (err,student)=>{
+        if(err) return next (err);
+        res.send("Student Updated!!!") 
+    })}   
--- studentAPI/routers/student_router.js
+++ studentAPI/routers/student_router.js
@@ -8,6 +8,8 @@ router.post("/create", students_controller.create)
 
 router.delete("/delete", students_controller.delete)
 
+router.put("/:id/put", students_controller.details)
+
 router.put("/:id/update", students_controller.update)
 
 module.exports = router;
